240d0b58e5bfee7543216bfb1959a53def4f65a3
Ziyodullo <ziyodullodev@gmail.com>
2023-09-08 23:25:27 +0500

    update: forum view, url, serializer

---
+++ apps/forum/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from . import models
+
+
+class OrganizationSerializer(ModelSerializer):
+    class Meta:
+        model = models.Organization
+        fields = "__all__"
+
+
+class ForumSerializer(serializers.ModelSerializer):
+    organization_title = serializers.CharField(source="organization.title", read_only=True)
+    organization_id = serializers.IntegerField(source="organization.id", read_only=True)
+
+    class Meta:
+        model = models.Forum
+        fields = "__all__"
+
+
+class ForumProjectSerializer(ModelSerializer):
+    forum_title = serializers.CharField(source="forum.title", read_only=True)
+    forum_id = serializers.IntegerField(source="forum.id", read_only=True)
+
+    class Meta:
+        model = models.ForumProject
+        fields = ("id", "forum_title", "forum_id", "title", "subtitle", "image", "context", "created", "modified")
+
+
+class EventSerializer(ModelSerializer):
+    forum_title = serializers.CharField(source="forum.title", read_only=True)
+    forum_id = serializers.IntegerField(source="forum.id", read_only=True)
+
+    class Meta:
+        model = models.Event
+        fields = ("id", "forum_title", "forum_id", "day", "date", "created", "modified")
+
+
+class EventTimeSerializer(ModelSerializer):
+    event_day = serializers.CharField(source="event.day", read_only=True)
+
+    class Meta:
+        model = models.EventTime
+        fields = "__all__"
---
+++ apps/forum/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Organization APIs
+    path('organizations/', views.OrganizationApiView.as_view(), name="organization"),
+    path('organization/<int:pk>/', views.OrganizationDetailApiView.as_view(), name="organization-detail"),
+
+    # Forum APIs
+    path('forum/create', views.ForumCreateApiView.as_view(), name="forum-create"),
+    path('forums/', views.ForumApiView.as_view(), name="forum-list"),
+    path('forum/<int:pk>/', views.ForumDetailApiView.as_view(), name="forum-detail"),
+
+    # ForumProject APIs
+    path('forum-project/create', views.ForumProjectCreateApiView.as_view(), name="forum-project-create"),
+    path('forum-projects/', views.ForumProjectListApiView.as_view(), name="forum-project-list"),
+    path('forum-project/<int:pk>/', views.ForumProjectDetailApiView.as_view(), name="forum-project-detail"),
+
+    # Event APIs
+    path('event/create', views.EventCreateApiView.as_view(), name="event-create"),
+    path('events/', views.EventApiView.as_view(), name="event-list"),
+    path('event/<int:pk>/', views.EventDetailApiView.as_view(), name="event-detail"),
+
+    # EventTime APIs
+    path('event-time/', views.EventTimeApiView.as_view(), name="event-time"),
+    path('event-time/<int:pk>/', views.EventTimeDetailApiView.as_view(), name="event-time-detail"),
+
+]
