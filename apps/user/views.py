9446962dfb251ba20be04df46f9152d3b2494d6a
d_karimov <d.karimov@uzinfocom.uz>
2023-09-09 19:38:47 +0500

    add random invitations generator

--- apps/user/views.py
+++ apps/user/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import Inivitation, User
 from .serializers import UserSerializer, InivitationSerializer
-
+import random
 
 class PaginationReport(PageNumberPagination):
     page_size = 10
@@ -20,20 +20,39 @@ class PaginationReport(PageNumberPagination):
 
 class InivitationApiView(APIView):
     serializer_class = InivitationSerializer
-    pagination_class = PaginationReport
 
     def get(self, request):
-        invitation = Inivitation.objects.all()
-        serializer = InivitationSerializer(instance=invitation, many=True)
+        invitations = Inivitation.objects.all()
+        serializer = InivitationSerializer(instance=invitations, many=True)
         return Response(serializer.data)
 
     def post(self, request):
         serializer = InivitationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            inputted_code = request.data.get('code')  # Get the inputted code
-            return Response({'code': inputted_code}, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def generate_invitations(self, quantity):
+        generated_invitations = []
+        for _ in range(quantity):
+            while True:
+                random_code = random.randint(100000, 9999999)  
+                if not Inivitation.objects.filter(code=random_code).exists():
+                    break
+            invitation = Inivitation(code=random_code)
+            invitation.save()
+            generated_invitations.append(invitation)
+        return generated_invitations
+
+    def post(self, request):
+        quantity = request.data.get('quantity')
+        if quantity is not None:
+            generated_invitations = self.generate_invitations(quantity)
+            serializer = InivitationSerializer(instance=generated_invitations, many=True)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'quantity': 'Quantity is required.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class InivitationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
