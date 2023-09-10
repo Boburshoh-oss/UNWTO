d310eff94c7912e3f47b29a62ba21b410d7e32eb
Ziyodullo <ziyodullodev@gmail.com>
2023-09-10 11:59:09 +0500

    update: urls, requirements, settings add staticfiles

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.staticfiles",
     'rest_framework',
     "django_filters",
     'drf_yasg',
