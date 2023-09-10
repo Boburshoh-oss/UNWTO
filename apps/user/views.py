ee562065d345e53c08bf59b8f745d9b2a76f5f9b
d_karimov <d.karimov@uzinfocom.uz>
2023-09-10 10:53:45 +0500

    bug fix

--- apps/user/views.py
+++ apps/user/views.py
@@ -11,6 +11,8 @@ from rest_framework.response import Response
 from .models import Inivitation, User
 from .serializers import UserSerializer, InivitationSerializer
 import random
+from django.shortcuts import get_object_or_404
+
 
 class PaginationReport(PageNumberPagination):
     page_size = 10
@@ -28,19 +30,11 @@ class InivitationApiView(APIView):
         result_page = paginator.paginate_queryset(invitations, request)
         serializer = InivitationSerializer(instance=result_page, many=True)
         return paginator.get_paginated_response(serializer.data)
-
-    def post(self, request):
-        serializer = InivitationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     def generate_invitations(self, quantity):
         generated_invitations = []
         for _ in range(quantity):
             while True:
-                random_code = random.randint(100000, 9999999)  
+                random_code = random.randint(100000, 9999999)
                 if not Inivitation.objects.filter(code=random_code).exists():
                     break
             invitation = Inivitation(code=random_code)
@@ -49,13 +43,41 @@ class InivitationApiView(APIView):
         return generated_invitations
 
     def post(self, request):
-        quantity = request.data.get('quantity')
+        quantity = request.data.get("quantity")
         if quantity is not None:
             generated_invitations = self.generate_invitations(quantity)
-            serializer = InivitationSerializer(instance=generated_invitations, many=True)
+            serializer = InivitationSerializer(
+                instance=generated_invitations, many=True
+            )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({'quantity': 'Quantity is required.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"quantity": "Quantity is required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class InvitationCheckApiView(APIView):
+    serializer_class = InivitationSerializer
+    pagination_class = PaginationReport
+
+    def get(self, request):
+        paginator = self.pagination_class()
+        invitations = Inivitation.objects.all()
+        result_page = paginator.paginate_queryset(invitations, request)
+        serializer = InivitationSerializer(instance=result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def post(self, request):
+        inputted_code = request.data.get('code')
+        if inputted_code is not None:
+            invitation = get_object_or_404(Inivitation, code=inputted_code)
+            if invitation.active:
+                serializer = InivitationSerializer(instance=invitation)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response({'code': 'Code already used.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'code': 'Code is required.'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class InivitationRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
