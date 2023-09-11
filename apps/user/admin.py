01449d9196897c413a2fdef7ce585adaeaffbfb0
d_karimov <d.karimov@uzinfocom.uz>
2023-09-11 23:54:37 +0500

    admin py fix

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -6,28 +6,46 @@ from .resources import UserResource
 
 
 # Register your models here.
-class InivitationAdmin(ExportActionMixin,admin.ModelAdmin):
+class InivitationAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ("code", "active")
     list_filter = ("created", "modified")
-    search_fields = ("code", )
+    search_fields = ("code",)
 
 
+from django.contrib.admin import SimpleListFilter
 
-class UserAdmin(ExportActionMixin,admin.ModelAdmin):
+
+class ForumTypeListFilter(SimpleListFilter):
+    title = 'Forum Type'
+    parameter_name = 'forum_type'
+
+    def lookups(self, request, model_admin):
+        return [(str(forum.id), forum.title) for forum in Forum.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(forum_type__id=self.value())
+
+
+class UserAdmin(ExportActionMixin, admin.ModelAdmin):
     resource_class = UserResource
-    
+
     list_display = (
         "first_name",
         "last_name",
         "date_of_birth",
         "country",
         "expire_date",
-
         "organization",
         "access_id",
         "invitation_id",
     )
-    list_filter = ("created", "modified","organization",("forum_type__title",MultiSelectFieldListFilter),("organization__title",MultiSelectFieldListFilter))
+    list_filter = (
+        "created",
+        "modified",
+        "organization",
+        ForumTypeListFilter,
+    )
     search_fields = (
         "first_name",
         "last_name",
@@ -38,7 +56,5 @@ class UserAdmin(ExportActionMixin,admin.ModelAdmin):
     )
 
 
-
 admin.site.register(Inivitation, InivitationAdmin)
 admin.site.register(User, UserAdmin)
-
