8022b78061827345e77c4d2987160bbc22c83fc9
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2023-09-11 18:47:17 +0500

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -174,7 +174,7 @@ MODELTRANSLATION_LANGUAGES = ('uz', 'en', 'ru')
 
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "../","media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "../","media/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
