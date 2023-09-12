a9194c818f4f09c9b0c94a2dca3169b8d4b8c683
Asilbek-MR <asilbekdavid@gmail.com>
2023-09-12 17:19:51 +0500

    add excel

--- apps/forum/admin.py
+++ apps/forum/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
-from import_export.admin import ExportActionMixin
 from .models import *
-from openpyxl import Workbook
+from openpyxl import Workbook, styles
 from openpyxl.drawing.image import Image
 from django.http import HttpResponse
 from django.utils.safestring import mark_safe
-from openpyxl.drawing.image import Image
+from openpyxl import Workbook
+
 
 # Forum admin
 class EventTabularInline(admin.TabularInline):
@@ -20,20 +20,6 @@ class ForumAdmin(TranslationAdmin):
     search_fields = ("title", "description", "organization__title")
     inlines = [EventTabularInline]
     actions = ["export_selected_to_excel"]
-
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ("title", "created", "modified")
-    list_filter = ("created", "modified")
-    search_fields = ("title",)
-    actions = ["export_selected_to_excel"]
-
-
-class ForumProjectAdmin(admin.ModelAdmin):
-    list_display = ("title", "subtitle", "context", "created", "modified")
-    list_filter = ("created", "modified")
-    search_fields = ("title", "subtitle", "context")
-    actions = ["export_selected_to_excel"]
-    
     
     def image_tag(self, obj):
         if obj.image:
@@ -50,11 +36,40 @@ class ForumProjectAdmin(admin.ModelAdmin):
         worksheet = workbook.active
 
         # Add headers to the Excel file
-        worksheet.append(["Title", "Subtitle", "Context", "Image"])
-
+        worksheet.append(["Title", "Short_key", "Organization", "Description"])
+        
         # Loop through the selected objects and add rows to the Excel file
+        image_style = styles.Font(size=60)
+
+        border = styles.Border(left=styles.Side(border_style=None,
+                                color='FF000000'),
+                        right=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        top=styles.Side(border_style=None,
+                                color='FF000000'),
+                        bottom=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal_direction=0,
+                        outline=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        vertical=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        horizontal=styles.Side(border_style=None,
+                                        color='FF000000')
+                        )
+        alignment= styles.Alignment(horizontal='general',
+                            vertical='bottom',
+                            text_rotation=0,
+                            wrap_text=False,
+                            shrink_to_fit=False,
+                            indent=0)
+        protection = styles.Protection(locked=True,
+                                hidden=False)
+        s = 2
         for project in queryset:
-            worksheet.append([project.title, project.subtitle, project.context, "Image"])
+            worksheet.append([project.title, project.subtitle, project.context, " "])
 
             # Add the image to the Excel file
             if project.image:
@@ -62,6 +77,12 @@ class ForumProjectAdmin(admin.ModelAdmin):
                 img.width = 100  # Adjust the image size as needed
                 img.height = 100
                 worksheet.add_image(img, "D{}".format(worksheet.max_row))  # Add image to column D
+                work_image = worksheet["D" + str(s)]
+                work_image.font = image_style
+            work_desc = worksheet["C" + str(s)]
+            s += 1
+            work_desc.alignment = alignment
+            work_desc.border = border
 
         # Create an HTTP response with the Excel file
         response = HttpResponse(content_type="application/ms-excel")
@@ -71,6 +92,160 @@ class ForumProjectAdmin(admin.ModelAdmin):
         workbook.save(response)
 
         return response
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("title", "created", "modified")
+    list_filter = ("created", "modified")
+    search_fields = ("title",)
+    actions = ["export_selected_to_excel"]
+    
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{}" width="50" height="50" />'.format(obj.image.url))
+        return "-"
+    
+    image_tag.short_description = "Image"
+
+    def export_selected_to_excel(self, request, queryset):
+        
+
+        # Create a new workbook and add a worksheet
+        workbook = Workbook()
+        worksheet = workbook.active
+
+        # Add headers to the Excel file
+        worksheet.append(["Title"])
+        
+        # Loop through the selected objects and add rows to the Excel file
+        image_style = styles.Font(size=60)
+
+        border = styles.Border(left=styles.Side(border_style=None,
+                                color='FF000000'),
+                        right=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        top=styles.Side(border_style=None,
+                                color='FF000000'),
+                        bottom=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal_direction=0,
+                        outline=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        vertical=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        horizontal=styles.Side(border_style=None,
+                                        color='FF000000')
+                        )
+        alignment= styles.Alignment(horizontal='general',
+                            vertical='bottom',
+                            text_rotation=0,
+                            wrap_text=False,
+                            shrink_to_fit=False,
+                            indent=0)
+        protection = styles.Protection(locked=True,
+                                hidden=False)
+        s = 2
+        for project in queryset:
+            worksheet.append([project.title, project.subtitle, project.context, " "])
+
+            # Add the image to the Excel file
+            if project.image:
+                img = Image(project.image.path)
+                img.width = 100  # Adjust the image size as needed
+                img.height = 100
+                worksheet.add_image(img, "D{}".format(worksheet.max_row))  # Add image to column D
+                work_image = worksheet["D" + str(s)]
+                work_image.font = image_style
+            work_desc = worksheet["C" + str(s)]
+            s += 1
+            work_desc.alignment = alignment
+            work_desc.border = border
+
+        # Create an HTTP response with the Excel file
+        response = HttpResponse(content_type="application/ms-excel")
+        response["Content-Disposition"] = "attachment; filename=forum_projects.xlsx"
+
+        # Save the workbook to the response
+        workbook.save(response)
+
+        return response
+
+
+class ForumProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "subtitle", "context", "created", "modified")
+    list_filter = ("created", "modified")
+    search_fields = ("title", "subtitle", "context")
+    actions = ["export_selected_to_excel"]
+    
+    
+
+
+    def export_selected_to_excel(self, request, queryset):
+        
+
+        # Create a new workbook and add a worksheet
+        workbook = Workbook()
+        worksheet = workbook.active
+
+        # Add headers to the Excel file
+        worksheet.append(["Forum","Title","Subtitle","Context","Image",])
+        
+        # Loop through the selected objects and add rows to the Excel file
+        image_style = styles.Font(size=60)
+
+        border = styles.Border(left=styles.Side(border_style=None,
+                                color='FF000000'),
+                        right=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        top=styles.Side(border_style=None,
+                                color='FF000000'),
+                        bottom=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal_direction=0,
+                        outline=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        vertical=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        horizontal=styles.Side(border_style=None,
+                                        color='FF000000')
+                        )
+        alignment= styles.Alignment(horizontal='general',
+                            vertical='bottom',
+                            text_rotation=0,
+                            wrap_text=False,
+                            shrink_to_fit=False,
+                            indent=0)
+        protection = styles.Protection(locked=True,
+                                hidden=False)
+        s = 2
+        for project in queryset:
+            worksheet.append([project.title, project.subtitle, project.context, " "])
+
+            # Add the image to the Excel file
+            if project.image:
+                img = Image(project.image.path)
+                img.width = 100  # Adjust the image size as needed
+                img.height = 100
+                worksheet.add_image(img, "D{}".format(worksheet.max_row))  # Add image to column D
+                work_image = worksheet["E" + str(s)]
+                work_image.font = image_style
+            work_desc = worksheet["C" + str(s)]
+            s += 1
+            work_desc.alignment = alignment
+            work_desc.border = border
+
+        # Create an HTTP response with the Excel file
+        response = HttpResponse(content_type="application/ms-excel")
+        response["Content-Disposition"] = "attachment; filename=forum_projects.xlsx"
+
+        # Save the workbook to the response
+        workbook.save(response)
+
+        return response
+
     
     
 
@@ -79,6 +254,70 @@ class EventTimeAdmin(admin.ModelAdmin):
     list_display = ("start_time", "end_time", "description")
     search_fields = ("start_time", "end_time", "description")
     actions = ["export_selected_to_excel"]
+    def export_selected_to_excel(self, request, queryset):
+        
+
+        # Create a new workbook and add a worksheet
+        workbook = Workbook()
+        worksheet = workbook.active
+
+        # Add headers to the Excel file
+        worksheet.append(["Start_time","End_time",'Description','Event'])
+        
+        # Loop through the selected objects and add rows to the Excel file
+        image_style = styles.Font(size=60)
+
+        border = styles.Border(left=styles.Side(border_style=None,
+                                color='FF000000'),
+                        right=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        top=styles.Side(border_style=None,
+                                color='FF000000'),
+                        bottom=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal_direction=0,
+                        outline=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        vertical=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        horizontal=styles.Side(border_style=None,
+                                        color='FF000000')
+                        )
+        alignment= styles.Alignment(horizontal='general',
+                            vertical='bottom',
+                            text_rotation=0,
+                            wrap_text=False,
+                            shrink_to_fit=False,
+                            indent=0)
+        protection = styles.Protection(locked=True,
+                                hidden=False)
+        s = 2
+        for project in queryset:
+            worksheet.append([project.title, project.subtitle, project.context, " "])
+
+            # Add the image to the Excel file
+            if project.image:
+                img = Image(project.image.path)
+                img.width = 100  # Adjust the image size as needed
+                img.height = 100
+                worksheet.add_image(img, "D{}".format(worksheet.max_row))  # Add image to column D
+                work_image = worksheet["D" + str(s)]
+                work_image.font = image_style
+            work_desc = worksheet["C" + str(s)]
+            s += 1
+            work_desc.alignment = alignment
+            work_desc.border = border
+
+        # Create an HTTP response with the Excel file
+        response = HttpResponse(content_type="application/ms-excel")
+        response["Content-Disposition"] = "attachment; filename=forum_projects.xlsx"
+
+        # Save the workbook to the response
+        workbook.save(response)
+
+        return response
 
 
 class EventTimeTabularInline(admin.TabularInline):
@@ -92,6 +331,77 @@ class EventAdmin(admin.ModelAdmin):
     search_fields = ("day", "date")
     inlines = [EventTimeTabularInline]
     actions = ["export_selected_to_excel"]
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{}" width="50" height="50" />'.format(obj.image.url))
+        return "-"
+    
+    image_tag.short_description = "Image"
+
+    def export_selected_to_excel(self, request, queryset):
+        
+
+        # Create a new workbook and add a worksheet
+        workbook = Workbook()
+        worksheet = workbook.active
+
+        # Add headers to the Excel file
+        worksheet.append(["Day","Date","Forum"])
+        
+        # Loop through the selected objects and add rows to the Excel file
+        image_style = styles.Font(size=60)
+
+        border = styles.Border(left=styles.Side(border_style=None,
+                                color='FF000000'),
+                        right=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        top=styles.Side(border_style=None,
+                                color='FF000000'),
+                        bottom=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal_direction=0,
+                        outline=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        vertical=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        horizontal=styles.Side(border_style=None,
+                                        color='FF000000')
+                        )
+        alignment= styles.Alignment(horizontal='general',
+                            vertical='bottom',
+                            text_rotation=0,
+                            wrap_text=False,
+                            shrink_to_fit=False,
+                            indent=0)
+        protection = styles.Protection(locked=True,
+                                hidden=False)
+        s = 2
+        for project in queryset:
+            worksheet.append([project.title, project.subtitle, project.context, " "])
+
+            # Add the image to the Excel file
+            if project.image:
+                img = Image(project.image.path)
+                img.width = 100  # Adjust the image size as needed
+                img.height = 100
+                worksheet.add_image(img, "D{}".format(worksheet.max_row))  # Add image to column D
+                work_image = worksheet["D" + str(s)]
+                work_image.font = image_style
+            work_desc = worksheet["C" + str(s)]
+            s += 1
+            work_desc.alignment = alignment
+            work_desc.border = border
+
+        # Create an HTTP response with the Excel file
+        response = HttpResponse(content_type="application/ms-excel")
+        response["Content-Disposition"] = "attachment; filename=forum_projects.xlsx"
+
+        # Save the workbook to the response
+        workbook.save(response)
+
+        return response
 
 
 # admin.site.register(Forum, ForumAdmin)
--- apps/user/admin.py
+++ apps/user/admin.py
@@ -3,6 +3,11 @@ from django_admin_multi_select_filter.filters import MultiSelectFieldListFilter
 from import_export.admin import ExportActionMixin
 from .models import *
 from .resources import UserResource
+import pycountry
+from openpyxl import Workbook, styles
+from openpyxl.drawing.image import Image
+from django.http import HttpResponse
+from openpyxl import Workbook
 
 
 # Register your models here.
@@ -27,7 +32,7 @@ class ForumTypeListFilter(SimpleListFilter):
             return queryset.filter(forum_type__id=self.value())
 
 
-class UserAdmin(ExportActionMixin, admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     resource_class = UserResource
 
     list_display = (
@@ -54,6 +59,81 @@ class UserAdmin(ExportActionMixin, admin.ModelAdmin):
         "access_id",
         "invitation_id",
     )
+    actions = ["export_selected_to_excel"]
+    
+    
+
+
+    def export_selected_to_excel(self, request, queryset):
+        
+
+        # Create a new workbook and add a worksheet
+        workbook = Workbook()
+        worksheet = workbook.active
+
+        # Add headers to the Excel file
+        worksheet.append(["Ismi","Familya","Tug'ulgan kuni","Davlati","email","Tugash muddati","Tashkilot","Kirish ID","Taklifnoma ID","Rasmi"," "])
+        
+        # Loop through the selected objects and add rows to the Excel file
+        image_style = styles.Font(size=60)
+
+        border = styles.Border(left=styles.Side(border_style=None,
+                                color='FF000000'),
+                        right=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        top=styles.Side(border_style=None,
+                                color='FF000000'),
+                        bottom=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        diagonal_direction=0,
+                        outline=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        vertical=styles.Side(border_style=None,
+                                    color='FF000000'),
+                        horizontal=styles.Side(border_style=None,
+                                        color='FF000000')
+                        )
+        alignment= styles.Alignment(horizontal='general',
+                            vertical='bottom',
+                            text_rotation=0,
+                            wrap_text=False,
+                            shrink_to_fit=False,
+                            indent=0)
+        protection = styles.Protection(locked=True,
+                                hidden=False)
+        s = 2
+        for project in queryset:
+            worksheet.append([project.first_name, project.last_name, project.date_of_birth, "Uzbekiston", project.email ,project.expire_date,project.organization.title, project.access_id,project.invitation_id.code, " "])
+            
+            # Add the image to the Excel file
+            if project.image:
+                img = Image(project.image.path)
+                img.width = 100  # Adjust the image size as needed
+                img.height = 100
+                worksheet.add_image(img, "J{}".format(worksheet.max_row))  # Add image to column D
+                work_image = worksheet["J" + str(s)]
+            
+                work_image.font = image_style
+            worksheet["C" + str(s)] = project.date_of_birth
+            country = str(project.country)
+            worksheet["D" + str(s)] = pycountry.countries.get(alpha_2=country.lower()).name
+
+            work_desc = worksheet["J" + str(s)]
+            s += 1
+            work_desc.alignment = alignment
+            work_desc.border = border
+
+        # Create an HTTP response with the Excel file
+        response = HttpResponse(content_type="application/ms-excel")
+        response["Content-Disposition"] = "attachment; filename=forum_projects.xlsx"
+
+        # Save the workbook to the response
+        workbook.save(response)
+
+        return response
+    
 
 
 admin.site.register(Inivitation, InivitationAdmin)
