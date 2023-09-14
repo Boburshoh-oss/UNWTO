49d0a72eb3b79b76daf57de3b988a3ac999ac51c
Boburshoh-oss <boburbekbotirov868@gmail.com>
2023-09-14 22:13:16 +0500

    contact

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
 from import_export.admin import ExportActionMixin
+from modeltranslation.admin import TranslationAdmin
+
 from .models import *
 
 # Register your models here.
@@ -8,10 +10,8 @@ admin.site.register(SocialMedia)
 admin.site.register(Banner)
 
 @admin.register(Contact)
-class ContactAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ("name",)
-    search_fields = ("name",)
+class ContactAdmin(TranslationAdmin, admin.ModelAdmin):
+    list_display = ("type","title","name","active")
+    search_fields = ("name","title")
+    list_filter = ("active","type")
 
-    formfield_overrides = {
-        models.TextField: {'widget': CKEditorWidget()},
-    }
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -17,9 +17,16 @@ class SocialMedia(models.Model):
     link = models.URLField()
     logo = models.FileField(upload_to="media/logo/")
 
-class Contact(models.Model):
-    name = models.CharField(max_length=20)
-    context = models.TextField()
+class Contact(TimeStampedModel,models.Model):
+    TYPES = (
+        (0, "Link"),
+        (1, "Email"),
+        (2, "Telefon"),
+    )
+    title = models.CharField(max_length=255, blank=True)
+    type =models.IntegerField(choices=TYPES, default=2)
+    name = models.CharField(max_length=20, blank=True)
+    active = models.BooleanField(default=True)
     
     def __str__(self) -> str:
-        return self.name
+        return self.title
