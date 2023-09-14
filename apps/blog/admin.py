a9f1859d53efb8a326aa0bb2972dc6ca78a070b7
Botirov Boburbek <74315658+Boburshoh-oss@users.noreply.github.com>
2023-09-14 13:11:10 +0000

    contact api

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-
+from ckeditor.widgets import CKEditorWidget
+from import_export.admin import ExportActionMixin
 from .models import *
 
 # Register your models here.
 admin.site.register(SocialMedia)
 admin.site.register(Banner)
-admin.site.register(Contact)
+
+@admin.register(Contact)
+class ForumAdmin(ExportActionMixin):
+    list_display = ("name",
+    search_fields = ("name",)
+
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget()},
+    }
